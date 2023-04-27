afe5909baa9ef545615cc39c3a91248597270364
Georgi Tangardzhiev <gkyshenko@abv.bg>
2023-04-28 01:08:14 +0300

    Day 8-9

--- functions/caesar_cipher/c_cipher_1.py
+++ functions/caesar_cipher/c_cipher_1.py
@@ -15,9 +15,34 @@ def encrypt(input_text, input_shift):
     print(f"The encrypted text is: {cipher_text}")
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("The shift number have to be less than 27. Anything above is considering too high for a cipher.\n"
-                  "Type the shift number:\n"))
+def decrypt(input_text, input_shift):
+    decrypted_text = ""
+    for letter in input_text:
+        position = alphabet.index(letter)
+        new_position = position - input_shift
+        if new_position < 0:
+            new_letter = alphabet[len(alphabet) - abs(new_position)]
+        else:
+            new_letter = alphabet[new_position]
+        decrypted_text += new_letter
+    print(f"The decrypted text is: {decrypted_text}")
 
-encrypt(text, shift)
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction == "encode":
+        text = input("Type your message:\n").lower()
+        shift = int(
+            input("The shift number have to be less than 27. Anything above is considering too high for a cipher.\n"
+                  "Type the shift number:\n"))
+        encrypt(text, shift)
+    elif direction == "decode":
+        text = input("Type your message:\n").lower()
+        shift = int(
+            input("Type the shift number:\n"))
+        decrypt(text, shift)
+    decision = input("If you want to continue press Y else, press N\n")
+    if decision == "Y".casefold():
+        continue
+    elif decision == "N".casefold():
+        break
