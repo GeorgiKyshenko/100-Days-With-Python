7d18cc7041599ac24573d0099501b845773cdbcb
Georgi Tangardzhiev <gkyshenko@abv.bg>
2023-05-01 02:57:37 +0300

    Day 8-9

--- loops/hangman/hangman_game.py
+++ loops/hangman/hangman_game.py
@@ -22,9 +22,6 @@ lives = 7
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    for position in range(len(chosen_word)):
-        if chosen_word[position] == guess:
-            display[position] = guess
     if guess not in chosen_word:
         print(f'You guessed "{guess}", that`s not in the word. You loose a point.\n{stages[lives - 1]}')
         lives -= 1
@@ -32,6 +29,9 @@ while not end_of_game:
             end_of_game = True
             print(f"You lost!\nThe word is: {concat_word(chosen_word)}")
     else:
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                display[position] = guess
         print(concat_word(display))
     if "_" not in display:
         end_of_game = True
