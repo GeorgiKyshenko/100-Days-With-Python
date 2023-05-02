215adbdcf015cda9c4046544278b8efc1e319323
Georgi Tangardzhiev <gkyshenko@abv.bg>
2023-05-02 22:21:36 +0300

    Day 10

---
+++ functions_with_outputs/docstrings.py
@@ -0,0 +1,7 @@
+# Giving description to a function. This text block can be also used as comment we call it with triple quotes """ """
+def separator():
+    """This is a simple separator between the lines of code"""
+    print("---------------")
+
+
+separator()
---
+++ functions_with_outputs/title_function.py
@@ -0,0 +1,6 @@
+def title_test(f_name, l_name):
+    print(f"{f_name.title()} {l_name.title()}")
+
+
+title_test("georgi", "KySheNko")
+
